5189ffa858c4f71f232fb9c5f9df87abda005b34
simon05091004 <dj05091004@apps.ntpc.edu.tw>
2026-05-21 15:58:38 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase 設定檔 - mathQuiz_hanlin_6a_u4u6
+// 請勿將此檔案分享給他人
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9lemHX8VEk7DiszkanLWGZjXB4TlCJKs",
+  authDomain: "math-5-6.firebaseapp.com",
+  projectId: "math-5-6",
+  storageBucket: "math-5-6.firebasestorage.app",
+  messagingSenderId: "791241827089",
+  appId: "1:791241827089:web:d33aec52c2bd1f59b322b0",
+  measurementId: "G-EW32BSYZKW"
+};
